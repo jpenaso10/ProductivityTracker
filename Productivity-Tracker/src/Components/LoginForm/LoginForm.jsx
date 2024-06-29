dd5dc4cc57bd372bca45eec24ef81975090ed5d8
JPENASO <84899665+jpenaso10@users.noreply.github.com>
2024-06-29 17:11:38 +0800

    [update]login form and registration form

--- Productivity-Tracker/src/Components/LoginForm/LoginForm.jsx
+++ Productivity-Tracker/src/Components/LoginForm/LoginForm.jsx
@@ -1,41 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginForm.css'
-import { FaUser,FaLock} from "react-icons/fa";
+import { FaUser,FaLock,FaEnvelope} from "react-icons/fa";
 
+const LoginForm = () => {
 
-function LoginForm() {
-  return (
+    const [action, setAction] = useState('');
 
-    <div className="wrapper">
-      
-    <form action="">
+    const registerLink = () => {
+        setAction('active');
+    };
 
-      <h1>Productivity Tracker</h1>
+    const loginLink = () => {
+        setAction('');
+    };
 
-      <div className="input-box">
-        <input type="text" placeholder='Username' required/>
-        <FaUser className='icon'/>
-      </div>
+    return (
 
-      <div className="input-box">
-        <input type="password" placeholder='Password' required />
-        <FaLock className='icon'/>
-      </div>
+        <div className={`wrapper ${action}`}>
 
-      <div className="remember-forgot">
-        <label><input type="checkbox" />Remember me</label>
-        <a href="#">Forgot password?</a>
-      </div>
+            <div className="form-box login">
+                <form action="">
+                    <h1>Productivity Tracker</h1>
+                    <div className="input-box">
+                        <input type="text" placeholder='Username' required/>
+                        <FaUser className='icon'/>
+                    </div>
 
-      <button type='submit'>Login</button>
+                    <div className="input-box">
+                        <input type="password" placeholder='Password' required />
+                        <FaLock className='icon'/>
+                    </div>
 
-      <div className="register-link">
-        <p>Don't have an account? <a href="#">Register</a></p>
-      </div>
+                    <div className="remember-forgot">
+                        <label><input type="checkbox" />Remember me</label>
+                        <a href="#">Forgot password?</a>
+                    </div>
 
-    </form>
-  </div>
-  );
+                    <button type='submit'>Login</button>
+
+                    <div className="register-link">
+                        <p>Don't have an account? <a href="#" onClick={registerLink}>Register</a></p>
+                    </div>
+                </form>
+            </div>
+
+            <div className="form-box register">
+                <form action="">
+                    <h1>Registration</h1>
+                    <div className="input-box">
+                        <input type="text" placeholder='Username' required/>
+                        <FaUser className='icon'/>
+                    </div>
+
+                    <div className="input-box">
+                        <input type="email" placeholder='E-mail' required/>
+                        <FaEnvelope className= 'icon' />
+                    </div>
+
+                    <div className="input-box">
+                        <input type="password" placeholder='Password' required />
+                        <FaLock className='icon'/>
+                    </div>
+
+                    <div className="remember-forgot">
+                        <label><input type="checkbox" />I agree to the terms & conditions</label>
+                    </div>
+
+                    <button type='submit'>Register</button>
+
+                    <div className="register-link">
+                        <p>Already have an account? <a href="#" onClick={loginLink}>Login</a></p>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default LoginForm
