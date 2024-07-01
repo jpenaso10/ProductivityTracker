8567646f1ddbd8c71afbd623a4ba345f8bbc6997
JPENASO <84899665+jpenaso10@users.noreply.github.com>
2024-07-01 19:04:43 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,9 @@
-
-
 const express = require('express')
-const connectDB = require('./db.js')
-const itemModel = require('./models/Item.js')
+
 const app = express()
 
-connectDB()
-
-app.listen(3000, () => {
-    console.log("app is running");
+app.post("/", async(req,res) => {
+    console.log("port Run");
 })
+
+app.listen(4000);
