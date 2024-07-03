66c7a3b31b182d6d23802878202ef5a5792b9810
JPENASO <84899665+jpenaso10@users.noreply.github.com>
2024-07-03 19:01:07 +0800

    Update Dashboard.jsx

--- Productivity-Tracker/src/Components/Pages/Dashboard/Dashboard.jsx
+++ Productivity-Tracker/src/Components/Pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Dashboard.css'
 import { FaUser, FaLock, FaEnvelope } from 'react-icons/fa'
+import { GrDashboard } from "react-icons/gr"
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
@@ -8,8 +9,17 @@ import { useNavigate } from 'react-router-dom'
 
 function Dashboard() {
   return (
-    <div className="nav-bar">
-        <h1>Dashboard</h1>
+    <div className="sidebar">
+      <div className="logo">
+        <ul className='main'>
+          <li>
+            <a href="#">
+              <i className='fas fa-tachometer-alt'><GrDashboard /></i>
+              <span>Dashboard</span>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
