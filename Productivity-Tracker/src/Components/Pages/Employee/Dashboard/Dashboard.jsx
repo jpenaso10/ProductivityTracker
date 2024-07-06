f3292caf0b2ddb0d3cc2eb644eafc1ca04ecb8a4
EarlLawrence7 <94208103+EarlLawrence7@users.noreply.github.com>
2024-07-06 19:10:35 +0800

    Update Dashboard.jsx

--- Productivity-Tracker/src/Components/Pages/Employee/Dashboard/Dashboard.jsx
+++ Productivity-Tracker/src/Components/Pages/Employee/Dashboard/Dashboard.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect } from 'react';
 import styles from './Dashboard.module.css'
 import { FaTachometerAlt, FaUser, FaChartBar, FaBriefcase, FaQuestionCircle, FaCog, FaSignOutAlt, FaSearch } from "react-icons/fa";
-import { BiAlarm } from "react-icons/bi";
 import { BiLogOut } from "react-icons/bi";
 import { CgProfile } from "react-icons/cg";
 import { CgMenuLeftAlt } from "react-icons/cg";
-import { CgMenuGridR } from "react-icons/cg";
 import { GoQuestion } from "react-icons/go";
 import { VscSettingsGear } from "react-icons/vsc";
 import { Navigate, useNavigate } from 'react-router-dom';
@@ -13,75 +11,74 @@ import axios from 'axios';
 
 function Dashboard() {
   const navigate = useNavigate()
-    axios.defaults.withCredentials = true
+  axios.defaults.withCredentials = true
 
-    /*useEffect(() => {
-        axios.get('http://localhost:5000/auth/verify')
-        .then(res=> {
-          if(res.data.status) {
+  /*useEffect(() => {
+      axios.get('http://localhost:5000/auth/verify')
+      .then(res=> {
+        if(res.data.status) {
 
-          } else {
-            navigate('/')
-          }
-        })
-    }, []) */
+        } else {
+          navigate('/')
+        }
+      })
+  }, []) */
 
   return (
-      <div>
-        <body>
-          <div className={styles.sidebar}>
-            <div className={styles.logo}></div>
-            <ul className={styles.menu}>
-              <li className={styles.active}>
-                <a href="./Dashboard">
-                  <CgMenuLeftAlt />
-                  <span>Dashboard</span>
-                </a>
-              </li>
-              <li>
-                <a href="/Profile">
-                  <CgProfile />
-                  <span>Profile</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <GoQuestion />
-                  <span>FAQ</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <VscSettingsGear />
-                  <span>Settings</span>
-                </a>
-              </li>
-              <li className={styles.logout}>
-                <a href="/">
+    <div>
+      <body>
+        <div className={styles.sidebar}>
+          <div className={styles.logo}></div>
+          <ul className={styles.menu}>
+            <li className={styles.active}>
+              <a href="./Dashboard">
+                <CgMenuLeftAlt />
+                <span>Dashboard</span>
+              </a>
+            </li>
+            <li>
+              <a href="/Profile">
+                <CgProfile />
+                <span>Profile</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <GoQuestion />
+                <span>FAQ</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <VscSettingsGear />
+                <span>Settings</span>
+              </a>
+            </li>
+            <li className={styles.logout}>
+              <a href="/">
+                <BiLogOut />
+                <span>Logout</span>
+              </a>
+            </li>
+          </ul>
+        </div>
 
-                  <BiLogOut />
-                  <span>Logout</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          <div className={styles.maincontent}>
-            <div className={styles.headerwrapper}>
-              <div className={styles.headertitle}>
-                <span>Primary</span>
-                <h2>Dashboard</h2>
-              </div>
-              <div className={styles.userinfo}>
-                <div className={styles.searchbox}>
-                  <FaSearch />
-                  <input type="text" placeholder="Search" />
-                </div>
+        <div className={styles.maincontent}>
+          <div className={styles.headerwrapper}>
+            <div className={styles.headertitle}>
+              <span>Primary</span>
+              <h2>Dashboard</h2>
+            </div>
+            <div className={styles.userinfo}>
+              <div className={styles.searchbox}>
+                <FaSearch />
+                <input type="text" placeholder="Search" />
               </div>
             </div>
           </div>
-        </body>
-      </div>
+        </div>
+      </body>
+    </div>
   );
 }
 
