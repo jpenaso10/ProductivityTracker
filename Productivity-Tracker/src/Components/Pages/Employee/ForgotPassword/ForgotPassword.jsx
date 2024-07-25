a6e63a8621c66c7cfa464868644e5ec05c3e8cfd
EarlLawrence7 <94208103+EarlLawrence7@users.noreply.github.com>
2024-07-25 13:45:57 +0800

    update/design

--- Productivity-Tracker/src/Components/Pages/Employee/ForgotPassword/ForgotPassword.jsx
+++ Productivity-Tracker/src/Components/Pages/Employee/ForgotPassword/ForgotPassword.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './ForgotPassword.css'
 import { FaUser, FaLock, FaEnvelope } from 'react-icons/fa'
 import axios from 'axios'
@@ -12,37 +12,37 @@ function ForgotPassword() {
     const handleSubmit = (e) => {
         e.preventDefault()
         axios.post("http://localhost:5000/auth/forgot-password", {
-            email, 
+            email,
         })
-        .then(response=> {
-            if(response.data.status) {
-                alert("Check your email for RESET password link")
-                navigate('/')
-            }
-            
-        })
-        .catch(err => {
-            console.log(err)
-        })
+            .then(response => {
+                if (response.data.status) {
+                    alert("Check your email for RESET password link")
+                    navigate('/')
+                }
+
+            })
+            .catch(err => {
+                console.log(err)
+            })
     }
 
-  return (
-    <div className="wrapper">
-        <div className="form-box login">
-            <form action="/" onSubmit={handleSubmit}>
-                <h1>Forgot Password</h1>
-                <h2>Enter the email associated with your account and well send you a link to reset your password.</h2>
-                
-                <div className="input-box">
-                    <input type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)} required />
-                    <FaEnvelope className='icon'/>
-                </div>
-     
-                <button type='submit'>Send</button>
-            </form>
+    return (
+        <div className="wrapper-forgot">
+            <div className="form-box login">
+                <form action="/" onSubmit={handleSubmit}>
+                    <h1>Forgot Password</h1>
+                    <h2>Enter the email associated with your account and well send you a link to reset your password.</h2>
+
+                    <div className="input-box">
+                        <input type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)} required />
+                        <FaEnvelope className='icon' />
+                    </div>
+
+                    <button type='submit'>Send</button>
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ForgotPassword
--- Productivity-Tracker/src/Components/Pages/Employee/Login/Login.jsx
+++ Productivity-Tracker/src/Components/Pages/Employee/Login/Login.jsx
@@ -56,6 +56,7 @@ function Login() {
       <div className="form-box login">
         <form onSubmit={handleSubmit}>
           <div className="logo-container">
+            <div className="logo-text">Productivity Tracker</div>
             <img src="/all-covered-svgrepo-com.png" alt="Productivity Tracker Logo" className="logo" />
           </div>
           <div className="form-group">
